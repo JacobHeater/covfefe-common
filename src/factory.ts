908294fbdbb965300d0f8882d1933b40fb6f185e
Jacob Heater <jacobheater@gmail.com>
2020-06-30 00:51:56 -0400

    Adding a doc for the factory function.

--- src/factory.ts
+++ src/factory.ts
@@ -1,3 +1,11 @@
+/**
+ * Provides a convenient means for instantiating
+ * and object with the given constructor, while allowing
+ * for initialization parameters to be given inline.
+ * 
+ * @param ctor The object constructor.
+ * @param params The initialization params.
+ */
 export function factory<T>(ctor: new () => T, params: T): T {
   if (!ctor) throw new Error(`Argument 'ctor' is required`);
 
